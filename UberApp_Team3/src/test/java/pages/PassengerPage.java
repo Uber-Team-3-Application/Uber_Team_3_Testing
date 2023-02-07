c21cc5fd3c3ea6edc87eb7f27c60fee71bdc8f12
Jelena Mikovic <jelena.mikovic189@gmail.com>
2023-02-07 02:33:08 +0100

    Added order a ride tests

--- UberApp_Team3/src/test/java/pages/PassengerPage.java
+++ UberApp_Team3/src/test/java/pages/PassengerPage.java
@@ -1,7 +1,7 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import helper.Helper;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,6 +14,28 @@ public class PassengerPage {
 
     @FindBy(css = "button[class='ride-button my-button']")
     private WebElement btnOrderRide;
+
+    @FindBy(id = "departure")
+    private WebElement departureInput;
+
+    @FindBy(id = "destination")
+    private WebElement destinationInput;
+
+    @FindBy(id = "selectVehicle")
+    private WebElement selectVehicle;
+
+    @FindBy(xpath = "//span[text()='STANDARD']")
+    private WebElement standardVehicle;
+
+    @FindBy(css = "button.my-button.ride-button")
+    private WebElement btnRideDetails;
+
+    @FindBy(css = "div.alert.alert-danger p")
+    WebElement errorMessageElement;
+
+
+    @FindBy(xpath = "//button[@type='button'][@class='ride-button my-button'][text()='Confirm']")
+    private WebElement confirm;
 
 
     public PassengerPage(WebDriver driver){
@@ -37,5 +59,68 @@ public class PassengerPage {
     public void logout(){
         (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.visibilityOf(btnLogout)).click();
+    }
+
+    public void orderARideInvalidInput() {
+        (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.visibilityOf(btnOrderRide)).click();
+        scrollIntoView();
+        (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.visibilityOf(btnRideDetails)).click();
+    }
+
+    private void scrollIntoView() {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", btnRideDetails);
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(btnRideDetails));
+    }
+
+    public String getWarningText() {
+        (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.visibilityOf(this.errorMessageElement));
+        return errorMessageElement.getText();
+    }
+
+    public void orderARide() {
+        (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.visibilityOf(btnOrderRide)).click();
+        scrollIntoView();
+
+        departureInput.sendKeys("Radnicka 1");
+        destinationInput.sendKeys("Radnicka 19");
+
+        (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.visibilityOf(selectVehicle)).click();
+
+        (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.visibilityOf(standardVehicle)).click();
+
+        (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.visibilityOf(btnRideDetails)).click();
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", btnRideDetails);
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOf(confirm));
+
+        Helper.takeScreenshoot(driver, "ride_details");
+        (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.visibilityOf(confirm)).click();
+        Helper.takeScreenshoot(driver, "ride_ordered");
+    }
+
+    public void orderARideNoDriver() {
+        try {
+            orderARide();
+        } catch(UnhandledAlertException  noe) {
+            Helper.takeScreenshoot(driver, "no_driver_found");
+            driver.switchTo().alert().accept();
+        }
+    }
+
+    public void refresh() {
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0,0)");
+        driver.navigate().refresh();
     }
 }
--- UberApp_Team3/src/test/java/tests/BaseTest.java
+++ UberApp_Team3/src/test/java/tests/BaseTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
@@ -12,12 +15,17 @@ import org.testng.annotations.BeforeTest;
 public class BaseTest {
 
     public static WebDriver driver;
+    public static WebDriver driver2;
 
     @BeforeSuite
     public void initDriver(){
 
         System.setProperty("WebDriver.chrome.driver",
                 "C:\\Users\\pc\\Desktop\\GIT\\Uber_Team_3_Testing\\UberApp_Team3\\chromedriver.exe");
+
+        System.setProperty("webdriver.edge.driver", "C:\\Users\\pc\\Desktop\\GIT\\Uber_Team_3_Testing\\UberApp_Team3\\msedgedriver.exe");
+        EdgeOptions options = new EdgeOptions();
+        driver2 = new EdgeDriver(options);
 
         driver = new ChromeDriver();
 
